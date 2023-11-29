68bc6452a7356e37b3c6d20f01d1ae4ed212a7e9
mouaddb <mouad10wolf@gmail.com>
2023-11-29 21:29:04 +0100

    Add node index page

---
+++ pages/nodes/index.tsx
@@ -0,0 +1,18 @@
+import { Nodes } from '@/components/node';
+import { GetServerSidePropsContext } from 'next';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import type { NextPageWithLayout } from 'types';
+
+const AllNodes: NextPageWithLayout = () => {
+  return <Nodes />;
+};
+
+export async function getStaticProps({ locale }: GetServerSidePropsContext) {
+  return {
+    props: {
+      ...(locale ? await serverSideTranslations(locale, ['common']) : {}),
+    },
+  };
+}
+
+export default AllNodes;
