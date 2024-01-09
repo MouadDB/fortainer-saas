75e391bfc3d82cb6e138648d631a217b3f8c3018
mouaddb <mouad10wolf@gmail.com>
2024-01-09 12:25:37 +0100

    Add common lib files

---
+++ lib/app.ts
@@ -0,0 +1,10 @@
+import packageInfo from '../package.json';
+
+const app = {
+  version: packageInfo.version,
+  name: 'Fortainer',
+  logoUrl: '',
+  url: 'http://localhost:4002',
+};
+
+export default app;
